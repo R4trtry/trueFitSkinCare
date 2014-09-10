7247b7622d6f83d104cc4e24a26976d1442d1fb3
Chang Liu <j8d8l5@gmail.com>
2014-09-10 09:50:41 -0700

    record error log for sql errors

--- data_preparing/data_dumping.py
+++ data_preparing/data_dumping.py
@@ -62,6 +62,7 @@ def insert_review():
 
   counter = 0
   error_count = 0
+  error_sql = []
   for category in folders:
     category_path = rootpath + category + '/'
     files = [f for f in os.listdir(category_path) if re.search('_',f) ]
@@ -90,40 +91,37 @@ def insert_review():
           db.rollback()
           if e.args[0] == 1062:
             #print 'Product {0} already in database...'.format(p[0])
-            continue
+            pass
           else:
             #if e.args[0] == 1366:
             #  sql1="""INSERT INTO Review(review_id,rating,quick_take,review_text,product_id,review_time,helpful,nohelpful) VALUES ('{0}',{1},'{2}','{3}','{4}','{5}',{6},'{7}')""".format(p[0],p[1],p[2],'',p[4],p[5],p[6],p[7])
             #  cursor.execute(sql1)
             #else:
             error_count+=1
-              #print sql1
-              #db.close()
-              #print e
-            continue
+            error_sql.append(sql1)
+            pass
 
         try:
           # Execute the SQL command
           cursor.execute(sql2)
           # Commit your changes in the database
           db.commit()
-          #if counter%1000 == 0:
-          #  print 'inserted ' + str(counter) + ' rows into Product...'
         except MySQLdb.Error as e:
           # Rollback in case there is any error
           db.rollback()
           if e.args[0] == 1062:
             print 'Review {0} already in database...'.format(p[0])
-            continue
+            pass
           else:
             error_count+=1
-            #print sql2
-            #db.close()
-            #print e
-            continue
+            error_sql.append(sql2)
+            pass
 
+  db.close()
   print error_count
-
+  f_err = open('sql_error_log.txt','w')
+  f_err.write('\n'.join(error_sql))
+  f_err.close()
 
 if __name__ == "__main__":
   main()
